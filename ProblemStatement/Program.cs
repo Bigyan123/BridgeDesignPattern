dccd12a6ad99532cb11b22bc5d37591805d1bb4a
bingyansa <bingyan.sahoo@intimetec.com>
2020-10-03 03:28:15 +0530

    code changes added

---
+++ ProblemStatement/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+
+namespace ProblemStatement
+{
+    public class ReadingApp
+    {
+        public string Text { get; set; }
+        public virtual void Display()
+        {
+            Console.WriteLine(Text);
+        }
+        public virtual void ReverseDisplay()
+        {
+            Console.WriteLine(new String(Text.Reverse().ToArray()));
+        }
+    }
+    public class Windows8App : ReadingApp
+    {
+        public override void Display()
+        {
+            Console.WriteLine("Display text in Windows 8");
+            base.Display();
+        }
+        public override void ReverseDisplay()
+        {
+            Console.WriteLine("Reverse text display in windows 8".Reverse());
+            base.ReverseDisplay();
+        }
+    }
+
+    public class Windows10App : ReadingApp
+    {
+        public override void Display()
+        {
+            Console.WriteLine("Display in windows 10");
+            base.Display();
+        }
+
+        public override void ReverseDisplay()
+        {
+            Console.WriteLine("Reverse display in windows 10".Reverse());
+            base.ReverseDisplay();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Class initialization
+            ReadingApp readingApp10 = new Windows10App() { Text = "Read this text for windows 10" };
+            readingApp10.Display();
+            //readingApp10.ReverseDisplay();
+
+            ReadingApp readingApp8 = new Windows8App() { Text = "Read this text for windows 10" };
+            readingApp8.Display();
+            //readingApp10.ReverseDisplay();
+
+            Console.Read();
+        }
+    }
+}
---
+++ Solution/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Linq;
+
+namespace Solution
+{
+    /// <summary>
+    /// The 'Implementor' abstract class
+    /// </summary>
+    public interface IDisplay
+    {
+        void Display(string Text);
+    }
+
+    /// <summary>
+    /// The 'ConcreteImplementor' class
+    /// </summary>
+    public class NormalDisplay:IDisplay
+    {
+        public void Display(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+
+    /// <summary>
+    /// The 'ConcreteImplementor' class
+    /// </summary>
+    public class ReverseDisplay:IDisplay
+    {
+        public void Display(string Text)
+        {
+            Console.WriteLine(new String(Text.Reverse().ToArray()));
+        }
+    }
+
+    /// <summary>
+    /// The 'Abstraction' class
+    /// </summary>
+    public abstract class ReadingText
+    {
+        protected IDisplay display;
+        public ReadingText(IDisplay display)
+        {
+            this.display = display;
+        }
+        public string Text { get; set; }
+        public abstract void Display();
+    }
+
+    /// <summary>
+    /// The 'RefinedAbstraction' class
+    /// </summary>
+    public class Windows8Reader:ReadingText
+    {
+        public Windows8Reader(IDisplay display):base(display)
+        {
+
+        }
+
+        public override void Display()
+        {
+            display.Display("This is for Windows8Reader\n" + Text);
+        }
+    }
+
+    /// <summary>
+    /// The 'RefinedAbstraction' class
+    /// </summary>
+    public class Windows10Reader:ReadingText
+    {
+        public Windows10Reader(IDisplay display):base(display)
+        {
+
+        }
+
+        public override void Display()
+        {
+            display.Display("This is for Windows10Reader\n" + Text);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create RefinedAbstraction
+            ReadingText readingText10 = new Windows10Reader(new NormalDisplay())
+            // Set ConcreteImplementor
+            { Text = "Read this text for Windows10" };
+            // Exercise the bridge
+            readingText10.Display();
+
+            ReadingText readingText8 = new Windows10Reader(new NormalDisplay()) { Text = "Read this text for Windows10" };
+            readingText8.Display();
+
+            //ReadingText readingText10R = new Windows10Reader(new ReverseDisplay()) { Text = "Read this text for Windows10" };
+            //readingText10R.Display();
+
+            //ReadingText readingText8R = new Windows10Reader(new ReverseDisplay()) { Text = "Read this text for Windows10" };
+            //readingText8R.Display();
+
+
+            Console.Read();
+        }
+    }
+}
